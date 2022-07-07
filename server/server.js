5d0d98b8e89ce64cdbb96f601bd1099e175984fa
Natalie Triffo <nattriffo@gmail.com>
2022-07-06 20:47:40 -0500

    deleting routes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
@@ -29,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-app.use(routes);
+
 
 db.once('open', () => {
   app.listen(PORT, () => {console.log(`🌍 Now listening on localhost:${PORT}`)
