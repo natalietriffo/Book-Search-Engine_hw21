bda85ee18ed6a30a411a70b92af935bcfb305b92
Natalie Triffo <nattriffo@gmail.com>
2022-07-12 18:27:17 -0500

    dashboard changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://natalietriffo:lipstick00@natalietriffo.x0l4z.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
